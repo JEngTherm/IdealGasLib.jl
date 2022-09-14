a2fc397c7fd9199e268243b2cb58ac2e927ac583
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2022-09-14 18:33:47 -0300

    UPD basic impl of nobleGasHeat

--- src/spHeat/nobleGas.jl
+++ src/spHeat/nobleGas.jl
@@ -2,5 +2,12 @@
 #                        Noble Gas Specific Heat Model for Ideal Gases                         #
 #----------------------------------------------------------------------------------------------#
 
+# Type declaration
+struct nobleGasHeat{𝗽,𝘅,𝗯<:IntBase} <: ConstHeat{𝗽,𝘅}
+    M::mAmt{𝗽,𝘅,MO}     # The precision- exactness- parametric molar mass
+    c::cpAmt{𝗽,𝘅,𝗯}     # The precision- exactness- base- parametric cp
+end
 
+# Type exporting
+export nobleGasHeat
 
