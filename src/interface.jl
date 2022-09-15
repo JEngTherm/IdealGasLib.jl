6c8d488d40e7a512912a1be1cdba595c5b0ddc3c
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2022-09-15 00:38:49 -0300

    ADD the src/interface.jl

---
+++ src/interface.jl
@@ -0,0 +1,15 @@
+#----------------------------------------------------------------------------------------------#
+#                                         interface.jl                                         #
+#----------------------------------------------------------------------------------------------#
+
+"""
+Function to return a particular substance's name.
+"""
+function name end
+
+"""
+Function to return a particular substance's chemical formula.
+"""
+function form end
+
+
