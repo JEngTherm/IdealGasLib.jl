1aaabc5850b3922742e4cc2e41471f48b11ae57b
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2022-09-15 11:16:37 -0300

    UPD adjusted the interface

--- src/interface.jl
+++ src/interface.jl
@@ -14,8 +14,18 @@ Function to return a particular substance's chemical formula.
 """
 function form end
 
+"""
+Function to return a particular substance's reference state's temperature.
+"""
+function Tref end
+
+"""
+Function to return a particular substance's reference state's specific entropy.
+"""
+function sref end
+
 
 # Interface exports
-export name, form
+export name, form, Tref, sref
 
 
