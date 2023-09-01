5dbe286cd7fd66d979fb866b33df6a903d351ff6
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2023-08-31 22:20:47 -0300

    UPD interface function's docs on interface.jl

--- src/interface.jl
+++ src/interface.jl
@@ -5,47 +5,47 @@
 # Interface (bare) functions
 
 """
-Function to return a particular substance's name.
+Function to return an ideal gas' name.
 """
 function name end
 
 """
-Function to return a particular substance's chemical formula.
+Function to return an ideal gas' chemical formula.
 """
 function form end
 
 """
-Function to return a particular substance's reference state's temperature.
+Function to return an ideal gas' reference state's temperature.
 """
 function Tref end
 
 """
-Function to return a particular substance's reference state's pressure.
+Function to return an ideal gas' reference state's pressure.
 """
 function Pref end
 
 """
-Function to return a particular substance's reference state's specific entropy.
+Function to return an ideal gas' reference state's specific entropy.
 """
 function sref end
 
 """
-Function to return a particular substance's variation of specific internal energy.
+Function to return an ideal gas' variation of specific internal energy.
 """
 function Δu end
 
 """
-Function to return a particular substance's variation of specific enthalpy.
+Function to return an ideal gas' variation of specific enthalpy.
 """
 function Δh end
 
 """
-Function to return a particular substance's variation of specific ideal gas partial entropy.
+Function to return an ideal gas' variation of specific ideal gas partial entropy.
 """
 function Δs° end
 
 """
-Function to return a particular substance's specific ideal gas partial entropy.
+Function to return an ideal gas' specific ideal gas partial entropy.
 """
 function s° end
 
